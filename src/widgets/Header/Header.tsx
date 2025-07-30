9917ff3f07cb866b7f9e2a23c0bad1aa346f9312
sadbatya <sadbatya@mail.ru>
2025-07-30 15:15:41 +0200

    feat: burger menu

--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -1,43 +1,69 @@
+import { useState } from "react";
 import style from "./Header.module.scss";
 import { Button } from "@/shared/ui";
+import clsx from "clsx";
 
-export const Header = () => (
-  <div className={style.header}>
-    <div className={style.logo}>
-      <img src="/logo.svg" alt="" />
+export const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <div className={style.header}>
+      <nav className={clsx(style.menu__mobile, !menuOpen && style.close)}>
+        <ul className={style.menu__mobile_list}>
+          <li className={style.menu__mobile_list_item}>Banking</li>
+          <li className={style.menu__mobile_list_item}>Card issuance</li>
+          <li className={style.menu__mobile_list_item}>Processing</li>
+          <li className={style.menu__mobile_list_item}>OTC deals</li>
+          <li className={style.menu__mobile_list_item}>About Us</li>
+        </ul>
+      </nav>
+      <div className={style.header__inner}>
+        <div className={style.burger__logo}>
+          <div
+            className={clsx(style.burger, menuOpen && style.active)}
+            onClick={() => setMenuOpen(!menuOpen)}
+          >
+            <span />
+          </div>
+          <div className={style.logo}>
+            <img src="/logo.svg" alt="" />
+          </div>
+        </div>
+
+        <nav className={style.navigation}>
+          <ul className={style.navigation__list}>
+            <li>
+              <a className={style.navigation__list_item} href="#">
+                Banking
+              </a>
+            </li>
+            <li>
+              <a className={style.navigation__list_item} href="#">
+                Card issuance
+              </a>
+            </li>
+            <li>
+              <a className={style.navigation__list_item} href="#">
+                Processing
+              </a>
+            </li>
+            <li>
+              <a className={style.navigation__list_item} href="#">
+                OTC deals
+              </a>
+            </li>
+            <li>
+              <a className={style.navigation__list_item} href="#">
+                About Us
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <div className={style.navigation__buttons}>
+          login
+          <Button className={style.button}>Open account</Button>
+        </div>
+      </div>
     </div>
-    <nav className={style.navigation}>
-      <ul className={style.navigation__list}>
-        <li>
-          <a className={style.navigation__list_item} href="#">
-            Banking
-          </a>
-        </li>
-        <li>
-          <a className={style.navigation__list_item} href="#">
-            Card issuance
-          </a>
-        </li>
-        <li>
-          <a className={style.navigation__list_item} href="#">
-            Processing
-          </a>
-        </li>
-        <li>
-          <a className={style.navigation__list_item} href="#">
-            OTC deals
-          </a>
-        </li>
-        <li>
-          <a className={style.navigation__list_item} href="#">
-            About Us
-          </a>
-        </li>
-      </ul>
-    </nav>
-    <div className={style.navigation__buttons}>
-      login
-      <Button className={style.button}>Open account</Button>
-    </div>
-  </div>
-);
+  );
+};
