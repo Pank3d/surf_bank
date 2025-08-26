9d51d6d0d0987540f18e4fd2b98e28e4be116a26
sadbatya <sadbatya@mail.ru>
2025-08-26 12:42:03 +0200

    feat: добавил админку и фронтенд в один репозиторий

---
+++ admin/src/api/lets-connect-page/controllers/lets-connect-page.ts
@@ -0,0 +1,7 @@
+/**
+ * lets-connect-page controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::lets-connect-page.lets-connect-page');
---
+++ admin/src/api/lets-connect-page/routes/lets-connect-page.ts
@@ -0,0 +1,7 @@
+/**
+ * lets-connect-page router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::lets-connect-page.lets-connect-page');
