dea04b6266260508666e7e73bafbe3ad44d553c3
copperwit <hrybou@yandex.ru>
2025-09-21 16:42:57 +0300

    feat(docker): init docker setup

--- frontend/src/widgets/Sections/Main/section-secure-our-business/section-secure-our-business.tsx
+++ frontend/src/widgets/Sections/Main/section-secure-our-business/section-secure-our-business.tsx
@@ -1,5 +1,5 @@
 import style from "./section-secure-our-business.module.scss";
-import img_1 from "@/assets/images/section-secure-our-business/img-1.webp";
+import img_1 from "@/assets/images/section-secure-our-business/Img-1.webp";
 import img_2 from "@/assets/images/section-secure-our-business/img-2.webp";
 import img_1_mobile from "@/assets/images/section-secure-our-business/img-1-mobile.webp";
 import { useEffect, useState } from "react";
